14c07d58a7df0ed55ee533ac62784ab410c8f759
Alejandro Martínez <alexito4@gmail.com>
2019-06-13 10:04:51 +0200

    public inits

--- Sources/EquallySpacedStack/EquallySpacedStack.swift
+++ Sources/EquallySpacedStack/EquallySpacedStack.swift
@@ -10,24 +10,24 @@ public struct EquallySpacedHStack: View {
     
     // MARK: TupleView support
     
-    init<A: View, B: View>(@ViewBuilder content: () -> TupleView<(A, B)>) {
+    public init<A: View, B: View>(@ViewBuilder content: () -> TupleView<(A, B)>) {
         let views = content().value
         self.items = [AnyView(views.0), AnyView(views.1)]
     }
     
-    init<A: View, B: View, C: View>(@ViewBuilder content: () -> TupleView<(A, B, C)>) {
+    public init<A: View, B: View, C: View>(@ViewBuilder content: () -> TupleView<(A, B, C)>) {
         let views = content().value
         self.items = [AnyView(views.0), AnyView(views.1), AnyView(views.2)]
     }
     
-    init<A: View, B: View, C: View, D: View>(@ViewBuilder content: () -> TupleView<(A, B, C, D)>) {
+    public init<A: View, B: View, C: View, D: View>(@ViewBuilder content: () -> TupleView<(A, B, C, D)>) {
         let views = content().value
         self.items = [AnyView(views.0), AnyView(views.1), AnyView(views.2), AnyView(views.3)]
     }
     
     // MARK: ForEach support
     
-    init<Data, Content: View>(@ViewBuilder content: () -> ForEach<Data, Content>) {
+    public init<Data, Content: View>(@ViewBuilder content: () -> ForEach<Data, Content>) {
         let views = content()
         self.items = views.data.map({ AnyView(views.content($0.identifiedValue)) })
     }
@@ -46,30 +46,30 @@ public struct EquallySpacedHStack: View {
 public struct EquallySpacedVStack: View {
     let items: [AnyView]
     
-    init<A: View>(@ViewBuilder content: () -> A) { // this init will be used for any non-supported number of TupleView
+    public init<A: View>(@ViewBuilder content: () -> A) { // this init will be used for any non-supported number of TupleView
         self.items = [AnyView(content())]
     }
     
     // MARK: TupleView support
     
-    init<A: View, B: View>(@ViewBuilder content: () -> TupleView<(A, B)>) {
+    public init<A: View, B: View>(@ViewBuilder content: () -> TupleView<(A, B)>) {
         let views = content().value
         self.items = [AnyView(views.0), AnyView(views.1)]
     }
     
-    init<A: View, B: View, C: View>(@ViewBuilder content: () -> TupleView<(A, B, C)>) {
+    public init<A: View, B: View, C: View>(@ViewBuilder content: () -> TupleView<(A, B, C)>) {
         let views = content().value
         self.items = [AnyView(views.0), AnyView(views.1), AnyView(views.2)]
     }
     
-    init<A: View, B: View, C: View, D: View>(@ViewBuilder content: () -> TupleView<(A, B, C, D)>) {
+    public init<A: View, B: View, C: View, D: View>(@ViewBuilder content: () -> TupleView<(A, B, C, D)>) {
         let views = content().value
         self.items = [AnyView(views.0), AnyView(views.1), AnyView(views.2), AnyView(views.3)]
     }
     
     // MARK: ForEach support
     
-    init<Data, Content: View>(@ViewBuilder content: () -> ForEach<Data, Content>) {
+    public init<Data, Content: View>(@ViewBuilder content: () -> ForEach<Data, Content>) {
         let views = content()
         self.items = views.data.map({ AnyView(views.content($0.identifiedValue)) })
     }
